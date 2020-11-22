cc3a45594ae48070c4234736618c98f033bec59d
VivianeGit <vivianecipriano.a@gmail.com>
2020-11-21 21:57:09 -0300

    Alterações feitas

--- marvelApp/app/src/main/java/com/example/marvelapp/comic/view/SearchComicFragment.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/comic/view/SearchComicFragment.kt
@@ -1,4 +1,4 @@
-package com.example.marvelapp.home.view
+package com.example.marvelapp.comic.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,17 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.marvelapp.R
 
+// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [SearchComicFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
-
+class SearchComicFragment : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -32,7 +33,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_search_comic, container, false)
     }
 
     companion object {
@@ -42,11 +43,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment SearchComicFragment.
          */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                HomeFragment().apply {
+                SearchComicFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
