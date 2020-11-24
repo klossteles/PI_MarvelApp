095fbf805465b3c3890a96ed6b526476d8c187fc
Giulia Machado <giuliamachado03@gmail.com>
2020-11-24 16:21:01 -0300

    Correção Endpoint creators

--- marvelApp/app/src/main/java/com/example/marvelapp/creator/repository/ICreatorsEndpoint.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/creator/repository/ICreatorsEndpoint.kt
@@ -8,12 +8,12 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ICreatorsEndpoint {
-    @GET("GET /v1/public/creators")
+    @GET("/v1/public/creators")
     suspend fun getCreators (@Query("nameStartsWith") nameStartsWith: String?,
                              @Query("offset") offset: Int? = 0
     ) : ResponseModel<CreatorsModel>
 
-    @GET("GET /v1/public/creators/{creatorId}")
+    @GET("/v1/public/creators/{creatorId}")
     suspend fun getCreatorById (@Path("creatorId") creatorId : Int
     ): ResponseModel<CreatorsModel>
 
