48a23addf2c3f6308c7be0d3079d30539c253d87
Ana Luiza <analuizaramalho9@gmail.com>
2020-11-25 16:01:02 -0300

    remove app bar botton and add navegation

---
+++ marvelApp/app/src/main/java/com/example/marvelapp/character/view/CharacterDescriptionFragment.kt
@@ -0,0 +1,46 @@
+package com.example.marvelapp.character.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.marvelapp.R
+
+class CharacterDescriptionFragment : Fragment() {
+    private lateinit var _view:View
+
+    private var _description:String?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            _description = it.getString("CHARACTER_DESCRIPTION")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character_description, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _view = view
+        _view.findViewById<TextView>(R.id.txtCharacterDescription).text=_description
+    }
+
+    companion object {
+
+        fun newInstance(description: String?) =
+            CharacterDescriptionFragment().apply {
+                arguments = Bundle().apply {
+                    putString("CHARACTER_DESCRIPTION", description)
+                }
+            }
+    }
+}
--- marvelApp/app/src/main/java/com/example/marvelapp/character/view/CharacterListFragment.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/character/view/CharacterListFragment.kt
@@ -59,9 +59,15 @@ class CharacterListFragment : Fragment() {
                 .navigate(R.id.action_characterListFragment_to_characterFragment, bundle)
         }
 
+<<<<<<< HEAD
 
         _recyclerView= _view.findViewById<RecyclerView>(R.id.listCharacters)
         _recyclerView.apply {
+=======
+        _recyclerView= _view.findViewById<RecyclerView>(R.id.listCharacters)
+
+        list.apply {
+>>>>>>> d6b10fa... remove app bar botton and add navegation
             setHasFixedSize(true)
             layoutManager = manager
             adapter = _listAdapter
