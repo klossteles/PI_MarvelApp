af1dfae748de599e743b986c5421848a2e89e7a7
Ana Luiza <analuizaramalho9@gmail.com>
2020-12-06 15:53:31 -0300

    Add lista de comics favoritas

--- marvelApp/app/src/main/java/com/example/marvelapp/favorite/view/FavoriteComicFragment.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/favorite/view/FavoriteComicFragment.kt
@@ -5,21 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.marvelapp.R
+import com.example.marvelapp.favorite.model.ComicFavoriteModel
+import com.example.marvelapp.favorite.repository.CharacterFavoriteRepository
+import com.example.marvelapp.favorite.repository.ComicFavoriteRepository
+import com.example.marvelapp.favorite.viewmodel.CharacterFavoriteViewModel
+import com.example.marvelapp.favorite.viewmodel.ComicFavoriteViewModel
 
 class FavoriteComicFragment : Fragment() {
 
+    private lateinit var _view:View
+    private lateinit var _viewModel: ComicFavoriteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_comic, container, false)
+        _view  = inflater.inflate(R.layout.fragment_favorite_comic, container, false)
+        return _view
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        fun newInstance() = FavoriteComicFragment()
+        _viewModel = ViewModelProvider(
+            this,
+            ComicFavoriteViewModel.ComicFavoriteViewModelFactory(ComicFavoriteRepository(_view.context))
+        ).get(ComicFavoriteViewModel::class.java)
+
+        _viewModel.listComicFavoriteData.observe(viewLifecycleOwner, Observer{
+            getList(it)
+        })
+
+        _viewModel.getComicFavorites()
+    }
+
+
+    private fun getList(list:List<ComicFavoriteModel>){
+        val viewManager= GridLayoutManager(_view.context,2)
+        val recyclerView=_view.findViewById<RecyclerView>(R.id.listComicFavorites)
+        val menuAdapter = ComicFavoriteAdapter(list){
+            Toast.makeText(context, "clicado", Toast.LENGTH_SHORT).show()
+        }
+
+        recyclerView.apply {
+            layoutManager=viewManager
+            adapter=menuAdapter
+        }
     }
 }
