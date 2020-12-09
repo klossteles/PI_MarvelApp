e9df9a83fa6274ab9d9e60e48dc745bd8fdb06ee
Ana Luiza <analuizaramalho9@gmail.com>
2020-12-08 22:26:56 -0300

    Add lista de criadores nos favoritos

---
+++ marvelApp/app/src/main/java/com/example/marvelapp/favorite/model/CreatorsFavoriteModel.kt
@@ -0,0 +1,6 @@
+package com.example.marvelapp.favorite.model
+
+data class CreatorsFavoriteModel (
+    val name:String,
+    val image:Int
+)
--- marvelApp/app/src/main/java/com/example/marvelapp/favorite/view/FavoriteCreatorFragment.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/favorite/view/FavoriteCreatorFragment.kt
@@ -5,56 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.marvelapp.R
+import com.example.marvelapp.favorite.model.CreatorsFavoriteModel
+import com.example.marvelapp.favorite.repository.CreatorsFavoriteRepository
+import com.example.marvelapp.favorite.viewmodel.CreatorsFavoriteViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteCreatorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoriteCreatorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var _view:View
+    private lateinit var _viewModel: CreatorsFavoriteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_creator, container, false)
+        _view  = inflater.inflate(R.layout.fragment_favorite_comic, container, false)
+        return _view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteCreatorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoriteCreatorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _viewModel = ViewModelProvider(
+            this,
+            CreatorsFavoriteViewModel.CreatorsFavoriteViewModelFactory(CreatorsFavoriteRepository(_view.context))
+        ).get(CreatorsFavoriteViewModel::class.java)
+
+        _viewModel.listCreatorsFavoriteData.observe(viewLifecycleOwner, Observer{
+            getList(it)
+        })
+
+        _viewModel.getCreatorsFavorites()
+    }
+
+    private fun getList(list:List<CreatorsFavoriteModel>){
+        val viewManager= GridLayoutManager(_view.context,2)
+        val recyclerView=_view.findViewById<RecyclerView>(R.id.listComicFavorites)
+        val menuAdapter = CreatorsFavoriteAdapter(list){
+            Toast.makeText(context, "clicado", Toast.LENGTH_SHORT).show()
+        }
+
+        recyclerView.apply {
+            layoutManager=viewManager
+            adapter=menuAdapter
+        }
     }
 }
