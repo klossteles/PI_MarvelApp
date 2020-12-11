2b8de2c54f2c4315a94d8b7dfbd84deec0ec2637
klossteles <klossteles@gmail.com>
2020-12-11 18:10:01 -0300

    Adiciona verificação se já está logado

--- marvelApp/app/src/main/java/com/example/marvelapp/login/view/LoginFragment.kt
+++ marvelApp/app/src/main/java/com/example/marvelapp/login/view/LoginFragment.kt
@@ -1,20 +1,20 @@
 package com.example.marvelapp.login.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import com.example.marvelapp.MainActivity
 import com.example.marvelapp.R
+import com.example.marvelapp.favorite.view.FavoritesActivity
 
 class LoginFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
-
+    private lateinit var _view:View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +23,29 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _view = view
+
+        val hide = arguments?.getBoolean(HIDE_BACK)
+        if (hide != null && hide) {
+            hideBackIcon()
+        }
+
+        _view.findViewById<Button>(R.id.containedButtonLogin).setOnClickListener {
+            val pref = _view.context.getSharedPreferences(MainActivity.MARVEL_APP, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(MainActivity.KEEP_LOGGED, true).apply()
+            val intent = Intent(_view.context, FavoritesActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+    }
+
+    private fun hideBackIcon() {
+        _view.findViewById<ImageView>(R.id.imgBackLogin).visibility = View.GONE
+    }
+
     companion object {
+        const val HIDE_BACK = "HIDE_BACK"
     }
 }
