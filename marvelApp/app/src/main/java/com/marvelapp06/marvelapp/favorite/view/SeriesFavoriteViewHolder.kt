ad29bdd96b966038ba85aecfdec970f5c97b115f
Ana Luiza <analuizaramalho9@gmail.com>
2021-01-21 17:22:27 -0300

    List favorites series

--- marvelApp/app/src/main/java/com/marvelapp06/marvelapp/favorite/view/SeriesFavoriteViewHolder.kt
+++ marvelApp/app/src/main/java/com/marvelapp06/marvelapp/favorite/view/SeriesFavoriteViewHolder.kt
@@ -6,16 +6,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.marvelapp06.marvelapp.R
 import com.marvelapp06.marvelapp.favorite.model.SeriesFavoriteModel
+import com.marvelapp06.marvelapp.series.model.SeriesModel
 import com.squareup.picasso.Picasso
 
 class SeriesFavoriteViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val _name = view.findViewById<TextView>(R.id.txtSeriesName)
     private val _image = view.findViewById<ImageView>(R.id.imgSeries)
 
-    fun bind(series:SeriesFavoriteModel) {
-        _name.text = series.name
+    fun bind(series: SeriesModel) {
+        _name.text = series.title
         Picasso.get()
-            .load(series.image)
+            .load(series.thumbnail?.getImagePath("landscape_incredible"))
             .into(_image)
     }
 }
