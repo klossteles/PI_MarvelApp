2fad74403e2cf7a5442de48c9576baa94955e670
klossteles <klossteles@gmail.com>
2021-02-01 23:01:44 -0300

    Fix verification if is connected even in favorites

--- marvelApp/app/src/main/java/com/marvelapp06/marvelapp/MainActivity.kt
+++ marvelApp/app/src/main/java/com/marvelapp06/marvelapp/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.marvelapp06.marvelapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.marvelapp06.marvelapp.favorite.view.FavoritesActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marvelapp06.marvelapp.character.view.CharacterFragment
 import com.marvelapp06.marvelapp.comic.view.ComicFragment
 import com.marvelapp06.marvelapp.creator.view.CreatorsFragment
+import com.marvelapp06.marvelapp.favorite.view.FavoritesActivity
 import com.marvelapp06.marvelapp.series.view.SeriesFragment
 import com.marvelapp06.marvelapp.utils.NetworkConnection
 import kotlinx.android.synthetic.main.activity_main.*
@@ -39,67 +39,76 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
+        val networkConnection = NetworkConnection(applicationContext)
         val bundle = intent.extras
         if (bundle != null) {
-            val frag = bundle.getString("KEY_FRAGMENT")
-            if (frag.toString() == "CharacterFragment") {
+            when (bundle.getString("KEY_FRAGMENT")) {
+                "CharacterFragment" -> {
+                    val manager = supportFragmentManager
+                    val transaction = manager.beginTransaction()
+                    val characterFragment = CharacterFragment()
 
-                val manager = supportFragmentManager
-                val transaction = manager.beginTransaction()
-                val characterFragment = CharacterFragment()
+                    characterFragment.arguments = intent.extras
+                    transaction.add(R.id.nav_host_fragment_container, characterFragment)
+                    transaction.commit()
+                    nav_host_fragment_container.visibility = View.VISIBLE
+                    layoutDisconnected.visibility = View.GONE
+                }
+                "SeriesFragment" -> {
+                    val manager = supportFragmentManager
+                    val transaction = manager.beginTransaction()
+                    val seriesFragment = SeriesFragment()
 
-                characterFragment.arguments = intent.extras
-                transaction.add(R.id.nav_host_fragment_container, characterFragment)
-                transaction.commit()
+                    seriesFragment.arguments = intent.extras
+                    transaction.add(R.id.nav_host_fragment_container, seriesFragment)
+                    transaction.commit()
+                    nav_host_fragment_container.visibility = View.VISIBLE
+                    layoutDisconnected.visibility = View.GONE
+                }
+                "ComicFragment" -> {
+                    val manager = supportFragmentManager
+                    val transaction = manager.beginTransaction()
+                    val comicFragment = ComicFragment()
 
-            } else if (frag.toString() == "SeriesFragment") {
+                    comicFragment.arguments = intent.extras
+                    transaction.add(R.id.nav_host_fragment_container, comicFragment)
+                    transaction.commit()
+                    nav_host_fragment_container.visibility = View.VISIBLE
+                    layoutDisconnected.visibility = View.GONE
+                }
+                "CreatorsFragment" -> {
+                    val manager = supportFragmentManager
+                    val transaction = manager.beginTransaction()
+                    val creatorsFragment = CreatorsFragment()
 
-                val manager = supportFragmentManager
-                val transaction = manager.beginTransaction()
-                val seriesFragment = SeriesFragment()
-
-                seriesFragment.arguments = intent.extras
-                transaction.add(R.id.nav_host_fragment_container, seriesFragment)
-                transaction.commit()
-
-            } else if (frag.toString() == "ComicFragment") {
-
-                val manager = supportFragmentManager
-                val transaction = manager.beginTransaction()
-                val comicFragment = ComicFragment()
-
-                comicFragment.arguments = intent.extras
-                transaction.add(R.id.nav_host_fragment_container, comicFragment)
-                transaction.commit()
-
-            } else if (frag.toString() == "CreatorsFragment") {
-
-                val manager = supportFragmentManager
-                val transaction = manager.beginTransaction()
-                val creatorsFragment = CreatorsFragment()
-
-                creatorsFragment.arguments = intent.extras
-                transaction.add(R.id.nav_host_fragment_container, creatorsFragment)
-                transaction.commit()
-
+                    creatorsFragment.arguments = intent.extras
+                    transaction.add(R.id.nav_host_fragment_container, creatorsFragment)
+                    transaction.commit()
+                    nav_host_fragment_container.visibility = View.VISIBLE
+                    layoutDisconnected.visibility = View.GONE
+                }
+                else -> {
+                    networkConnection.observe(this, Observer { isConnected ->
+                        if (isConnected) {
+                            nav_host_fragment_container.visibility = View.VISIBLE
+                            layoutDisconnected.visibility = View.GONE
+                        } else {
+                            nav_host_fragment_container.visibility = View.GONE
+                            layoutDisconnected.visibility = View.VISIBLE
+                        }
+                    })
+                }
             }
+        } else {
+            networkConnection.observe(this, Observer { isConnected ->
+                if (isConnected) {
+                    nav_host_fragment_container.visibility = View.VISIBLE
+                    layoutDisconnected.visibility = View.GONE
+                } else {
+                    nav_host_fragment_container.visibility = View.GONE
+                    layoutDisconnected.visibility = View.VISIBLE
+                }
+            })
         }
-
-        val networkConnection = NetworkConnection(applicationContext)
-        networkConnection.observe(this, Observer { isConnected ->
-            if (isConnected) {
-                nav_host_fragment_container.visibility = View.VISIBLE
-                layoutDisconnected.visibility = View.GONE
-            } else {
-                nav_host_fragment_container.visibility = View.GONE
-                layoutDisconnected.visibility = View.VISIBLE
-            }
-        })
-    }
-
-    companion object {
-        const val MARVEL_APP = "MarvelApp"
-        const val KEEP_LOGGED = "KeepLogged"
     }
 }
