8d48633161b92b97051eceb3f061c50c7ba45403
VivianeGit <vivianecipriano.a@gmail.com>
2021-02-04 16:15:31 -0300

    Adiciona Constants (Quiz)

--- marvelApp/app/src/main/java/com/marvelapp06/marvelapp/quiz/QuizQuestionFragment.kt
+++ marvelApp/app/src/main/java/com/marvelapp06/marvelapp/quiz/QuizQuestionFragment.kt
@@ -1,28 +1,69 @@
 package com.marvelapp06.marvelapp.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import com.marvelapp06.marvelapp.R
+import kotlinx.android.synthetic.main.fragment_quiz_question.*
 
 class QuizQuestionFragment : Fragment() {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
     private lateinit var _view: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+
+            mQuestionsList = Constants.getQuestions()
+
+            setQuestion()
+
+    }
+
+    private fun setQuestion() {
+
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        progressBarQuestions.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBarQuestions.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+        tv_option_five.text = question.optionFive
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+        options.add(4, tv_option_five)
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz_question, container, false)
@@ -34,7 +75,7 @@ class QuizQuestionFragment : Fragment() {
 
         _view.findViewById<ImageButton>(R.id.btnNextQuestionQuiz).setOnClickListener {
             _view.findNavController()
-                .navigate(R.id.action_quizQuestionFragment_to_quizResultFragment)
+                    .navigate(R.id.action_quizQuestionFragment_to_quizResultFragment)
         }
     }
 }
